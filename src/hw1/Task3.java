37a067beda2543ea4157809700ab99a7a2edf2d5
Art1shz <124880597+Art1shz@users.noreply.github.com>
2024-01-12 00:55:57 +0300

    Task3.java

--- src/hw1/Task3.java
+++ src/hw1/Task3.java
@@ -2,14 +2,17 @@ package hw1;
 import java.util.Scanner;
 
 public class Task3 {
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Введите год в диапазоне от 1500 до 2024:");
         int year = scanner.nextInt();
         int century = (year - 1) / 100 + 1;
-        if (century >= 15 && century <= 21) {
+        if (century >= 15 && century <= 21) 
+        {
             System.out.println("Век = " + century);
-        } else {
+        } else 
+        {
             System.out.println("Данный год не входит в диапазон");
         }
     }
